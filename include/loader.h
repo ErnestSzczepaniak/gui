cc65acee5815360954c919d90d5fd32eb7e7e7b8
Ernest <ernest.szczepaniak.en2@gmail.com>
2019-10-10 15:24:52 +0200

    asd

--- include/loader.h
+++ include/loader.h
@@ -10,57 +10,66 @@ struct Model
     int count;
 };
 
-struct Texture_cell
-{
-    bool empty = true;
-    Texture texture;
-};
-
-struct [[gnu::packed]] Header
-{
-    unsigned short int id;
-    unsigned int size;
-    unsigned short int reserved[2];
-    unsigned int offset;
-    unsigned int bytes_in_header;
-    unsigned int width;
-    unsigned int height;
-    unsigned short int planes;
-    unsigned short int bits_per_pixel;
-    unsigned int bi_rgb;
-    unsigned int size_raw;
-};
-
-struct Model_info
-{
-    int rows;
-    int cols;
-    int count;
-};
-
-struct File_info
-{
-    int width;
-    int height;
-    int offset;
-};
-
 class Loader
 {
+    struct Model_info
+    {
+        int rows;
+        int cols;
+        int count;
+    };
+
+    struct File_info
+    {
+        int width;
+        int height;
+        int offset;
+    };
+
+    struct Texture_info
+    {
+        int row;
+        int col;
+        int start;
+    };
+
+    struct Texture_cell
+    {
+        bool empty = true;
+        Texture texture;
+    };
+
+    struct [[gnu::packed]] Header
+    {
+        unsigned short int id;
+        unsigned int size;
+        unsigned short int reserved[2];
+        unsigned int offset;
+        unsigned int bytes_in_header;
+        unsigned int width;
+        unsigned int height;
+        unsigned short int planes;
+        unsigned short int bits_per_pixel;
+        unsigned int bi_rgb;
+        unsigned int size_raw;
+    };
+
 public:
     Loader();
     ~Loader();
 
     Texture * load(const char * path, int index);
+    Model load(const char * path);
 
 protected:
     File_info _file_info();
     Model_info _model_info(int width, int height);
+    Texture_info _texture_info(int index, int rows, int cols, int offset);
 
     Texture_cell * _cell_empty_find();
 
-
     void _load_from_offset(int offset);
+    void _texture_link(Texture * previous, Texture * next);
 
 private:
     Texture_cell _cell[loader_cell_size];
--- source/loader.cpp
+++ source/loader.cpp
@@ -14,29 +14,27 @@ Loader::~Loader()
 
 Texture * Loader::load(const char * path, int index)
 {
-    if (auto result = h::gui::file_open(path))
+    if (auto result = h::gui::file_open(path); result == true)
     {
         auto [width, height, offset] = _file_info();
         auto [rows, cols, count] = _model_info(width, height);
         
         if (index < count)
         {
-            auto texture_row = index / cols;
-            auto texture_col = index % cols;
-            auto row_end = offset + (rows - texture_row - 1) * texture_size_bytes * cols;
-            auto row_start = row_end + texture_size_bytes * cols - (cols - texture_col) * texture_size_x_bytes;
+            auto [row, col, start] = _texture_info(index, rows, cols, offset);
 
             auto * cell = _cell_empty_find();
 
             for (int i = 0; i < texture_size_y_pixels; i++)
             {
-                _load_from_offset(row_start);
+                _load_from_offset(start);
 
                 memcpy(cell->texture.pixel(0, i), _buffer, texture_size_x_bytes);
 
-                row_start -= cols * texture_size_x_bytes;
+                start -= cols * texture_size_x_bytes;
             }
 
+            h::gui::file_close();
             return &cell->texture;
         }
         else
@@ -51,9 +49,40 @@ Texture * Loader::load(const char * path, int index)
     }    
 }
 
+Model Loader::load(const char * path)
+{
+    int index = 0;
+    Texture * current;
+    Texture * previous;
+    Texture * first;
+
+    while(current = load(path, index))
+    {
+        if (index == 0)
+        {
+            current->previous(nullptr);
+            first = current;
+        }
+        else _texture_link(previous, current);
+
+        index++;
+
+        previous = current;
+    }
+
+    if (index)
+    {
+        return {first, index};
+    }
+    else
+    {
+        return {nullptr, 0};
+    }
+}
+
 //---------------------------------------------| info |---------------------------------------------//
 
-File_info Loader::_file_info()
+Loader::File_info Loader::_file_info()
 {
     h::gui::file_read(_buffer, sizeof(Header));
 
@@ -62,7 +91,7 @@ File_info Loader::_file_info()
     return {ptr->width, ptr->height, ptr->offset};
 }
 
-Model_info Loader::_model_info(int width, int height)
+Loader::Model_info Loader::_model_info(int width, int height)
 {
     auto rows = height / texture_size_y_pixels;
     auto cols = width / texture_size_x_pixels;
@@ -77,7 +106,7 @@ void Loader::_load_from_offset(int offset)
     h::gui::file_read(_buffer, 64);
 }
 
-Texture_cell * Loader::_cell_empty_find()
+Loader::Texture_cell * Loader::_cell_empty_find()
 {
     for (int i = 0; i < loader_cell_size; i++)
     {
@@ -89,4 +118,20 @@ Texture_cell * Loader::_cell_empty_find()
     }
 
     return nullptr;
+}
+
+Loader::Texture_info Loader::_texture_info(int index, int rows, int cols, int offset)
+{
+    auto texture_row = index / cols;
+    auto texture_col = index % cols;
+    auto row_end = offset + (rows - texture_row - 1) * texture_size_bytes * cols;
+    auto row_start = row_end + texture_size_bytes * cols - (cols - texture_col) * texture_size_x_bytes;
+
+    return {texture_row, texture_col, row_start};
+}
+
+void Loader::_texture_link(Texture * previous, Texture * next)
+{
+    previous->next(next);
+    next->previous(previous);
 }
